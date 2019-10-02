ab342c51b63188bc5bbd639f2c6d4bae1b5057fc
Peter Brown <peter.brown.16@ucl.ac.uk>
2019-10-02 11:10:20 +0100

    Added five data type variables and printed them

--- PHAS0056/src/module1/DataTypes.java
+++ PHAS0056/src/module1/DataTypes.java
@@ -3,8 +3,17 @@ package module1;
 public class DataTypes {
 
 	public static void main(String[] args) {
+		double doubVar = 5.0;
+		System.out.println("doubVar equals " +doubVar);
+		float floatVar = 5;
+		System.out.println("floatVar equals " +floatVar);
+		int intVar = 5;
+		System.out.println("intVar equals " +intVar);
+		long longVar = 5;
+		System.out.println("longVar equals " +longVar);
+		byte byteVar = 5;
+		System.out.println("byteVar equals " +byteVar);
 		
-
 	}
 
 }
