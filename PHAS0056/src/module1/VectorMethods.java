5c9c0fb90d7382c941fe00695505b829db192048
Peter Brown <peter.brown.16@ucl.ac.uk>
2019-10-04 13:04:42 +0100

    Defined new dot product method for two 3D vectors

--- PHAS0056/src/module1/VectorMethods.java
+++ PHAS0056/src/module1/VectorMethods.java
@@ -1,21 +1,22 @@
 package module1;
+import java.lang.Math.*;
 
 public class VectorMethods {
+	public VectorMethods() { }
 	
+	public double dotProduct(double x, double y, double z, double a, double b, double c) {
+	double dotOutput;
+	dotOutput = (x*a)+(y*b)+(z*c);
+	return dotOutput;
 	
+	}
 	
-
 	public static void main(String[] args) {
-		
+	
 	VectorMethods vm = new VectorMethods();
-		
-	double x;
-	double y;
-	double z;
+	System.out.println("The dot product of the vectors (1,2,3) and (4,5,6) is:" +vm.dotProduct(1,2,3,4,5,6));
+	System.out.println("\n");	
 	
-	
-		
-		
 		
 		
 		
