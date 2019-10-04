cd6cc506139c9ce54c76a2897e6544cd0f4d2149
Peter Brown <peter.brown.16@ucl.ac.uk>
2019-10-04 12:06:38 +0100

    Created VectorMethods class and created vm object

---
+++ PHAS0056/src/module1/VectorMethods.java
@@ -0,0 +1,24 @@
+package module1;
+
+public class VectorMethods {
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+	VectorMethods vm = new VectorMethods();
+		
+	double x;
+	double y;
+	double z;
+	
+	
+		
+		
+		
+		
+		
+	}
+
+}
