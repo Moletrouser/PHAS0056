89804ae9992593b46b55ef15d104771859f324c0
Peter Brown <peter.brown.16@ucl.ac.uk>
2019-10-04 13:32:26 +0100

    added magnitude function and tested it

--- PHAS0056/src/module1/VectorMethods.java
+++ PHAS0056/src/module1/VectorMethods.java
@@ -8,16 +8,24 @@ public class VectorMethods {
 	double dotOutput;
 	dotOutput = (x*a)+(y*b)+(z*c);
 	return dotOutput;
-	
 	}
+	
+	public double magnitude(double x, double y, double z) {
+	double magnitudeOutput;
+	magnitudeOutput = Math.sqrt((x*x)+(y*y)+(z*z));
+	return magnitudeOutput;
+	
+}
 	
 	public static void main(String[] args) {
 	
 	VectorMethods vm = new VectorMethods();
-	System.out.println("The dot product of the vectors (1,2,3) and (4,5,6) is:" +vm.dotProduct(1,2,3,4,5,6));
+	
+	System.out.println("The dot product of the vectors (1,2,3) and (4,5,6) is: " +vm.dotProduct(1,2,3,4,5,6));
 	System.out.println("\n");	
 	
-		
+	System.out.println("The magnitude of the vector (1,2,3) is: " +vm.magnitude(1, 2, 3));
+	System.out.println("\n");	
 		
 		
 	}
