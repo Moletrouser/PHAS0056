e66218ae8d2bdfbf8df123d27034212adba202bf
Moletrouser <peter8rown@hotmail.co.uk>
2019-10-05 22:15:44 +0100

    Define the 'angle' function and tested it's outputs with explanation

--- PHAS0056/src/module1/VectorMethods.java
+++ PHAS0056/src/module1/VectorMethods.java
@@ -14,8 +14,17 @@ public class VectorMethods {
 	double magnitudeOutput;
 	magnitudeOutput = Math.sqrt((x*x)+(y*y)+(z*z));
 	return magnitudeOutput;
+	}
 	
-}
+	public double angle(double x, double y, double z, double a, double b, double c) {
+	double angleOutput;
+	angleOutput = Math.toDegrees(Math.acos((dotProduct(x,y,z,a,b,c)/(magnitude(x,y,z)*magnitude(a,b,c)))));
+	return angleOutput;
+	
+		
+	}
+
+
 	
 	public static void main(String[] args) {
 	
@@ -27,7 +36,10 @@ public class VectorMethods {
 	System.out.println("The magnitude of the vector (1,2,3) is: " +vm.magnitude(1, 2, 3));
 	System.out.println("\n");	
 		
-		
+	System.out.println("The angle between the vectors (4,3,2) and (1,5,4) in degrees is: " +vm.angle(4,3,2,1,5,4));
+	System.out.println("The angle between the vectors (4,3,2) and (0,0,0) in degrees is: " +vm.angle(4,3,2,0,0,0));
+	System.out.println("The angle is given as 'NaN' since (0,0,0) is the zero vector, since it has no length or direction the angle between it and any other vector cannot be defined");
+	System.out.println("\n");
 	}
 
 }
