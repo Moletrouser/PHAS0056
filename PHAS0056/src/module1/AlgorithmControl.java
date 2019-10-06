679cf4aaa5d4a3ff123c6e3832672ddde683005a
Peter Brown <peter.brown.16@ucl.ac.uk>
2019-10-06 17:39:34 +0100

    Finished 'count to 8' loop and tested it

--- PHAS0056/src/module1/AlgorithmControl.java
+++ PHAS0056/src/module1/AlgorithmControl.java
@@ -1,16 +1,38 @@
 package module1;
 
 public class AlgorithmControl {
-	public AlgorithmControl() { }
 	
+	public void loop(int imin, int imax) {
+		int i = 0;
+		for (i = imin; i <= imax; i=i+1) {
+		System.out.println(i); }
+		return; 
 	
-	
-	
+}
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		AlgorithmControl ac = new AlgorithmControl();
+	
+		System.out.println("Counting from 1 to 8:");
+		ac.loop(1, 8);
+		
+	
+		//int i = 1;
+		//int maxI = 9;
+		//for (;i < maxI; i++) {
+		//System.out.println(" i = " +i);
+	
+	
+	
 	}
-
+		//System.out.println("\n");
+	
+		//int n = 10;
+		//int minN = -6;
+	
 }
+ 
+	
+//}
