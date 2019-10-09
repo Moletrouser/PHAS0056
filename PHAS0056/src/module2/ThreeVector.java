f706f6bb9c7f550392939b8cd90a7aad4bc19c8f
Peter Brown <peter.brown.16@ucl.ac.uk>
2019-10-09 12:52:09 +0100

    finished static and non static scalar product functions

--- PHAS0056/src/module2/ThreeVector.java
+++ PHAS0056/src/module2/ThreeVector.java
@@ -4,7 +4,8 @@ public class ThreeVector {
 	
 	double x;
 	double y;
-	double z;
+    double z;
+
 	
 	public ThreeVector(double x, double y, double z) {
 		this.x = x; 
@@ -12,11 +13,7 @@ public class ThreeVector {
 		this.z = z;
 	}
 	
-	
-	
-	
 		public double magnitude() {
-		
 			double magnitudeOutput;
 			magnitudeOutput = Math.sqrt((x*x)+(y*y)+(z*z));
 			return magnitudeOutput;
@@ -34,20 +31,51 @@ public class ThreeVector {
 			return stringVector;
 	}
 
+	public static double scalarProduct(ThreeVector vector1, ThreeVector vector2) {
+		double scalarOutput;
+		scalarOutput = vector1.x*vector2.x + vector1.y*vector2.y + vector1.z*vector2.z;
+		return scalarOutput;
 	
+	}
+	
+	public double scalarProduct(ThreeVector v2) {
+	
+		return scalarProduct(this,v2);
+	}
+	
+  //  public ThreeVector vectorProduct(ThreeVector vector2) {
+  //  double vectorOutputx = (y*c)-(z*b);
+  //  double vectorOutputy = (z*a)-(x*c);
+  //  double vectorOutputz = (x*b)-(y*a);
+  // ThreeVector vectorOutput = new ThreeVector(vectorOutputx, vectorOutputy, vectorOutputz);
+  //  return vectorOutput;
+  // }
 	
 	public static void main(String[] args) {
 		
-		ThreeVector tv = new ThreeVector(1, 2, 3);
+		ThreeVector tv1 = new ThreeVector(1, 2, 3);
+		ThreeVector tv2 = new ThreeVector(4, 5, 6);
+		double answer = tv1.scalarProduct(tv2);
 	
-		System.out.println("The magnitude of the (1,2,3) vector is: " +tv.magnitude() );
+		System.out.println("The magnitude of the (1,2,3) vector is: " +tv1.magnitude() );
 		System.out.println("\n");
 		
-		System.out.println("The unit vector of the (1,2,3) vector is: " +tv.unitVector() );
+		System.out.println("The unit vector of the (1,2,3) vector is: " +tv1.unitVector() );
 		System.out.println("\n");
 		
-		System.out.println("The original vector is: " +tv );
+		System.out.println("The original vector is: " +tv1 );
 	    System.out.println("\n");
+	    
+	  //  System.out.println(tv.vectorProduct(1,2,3,4,5,6));
+	    
+	    double x = 1;
+	    double y = 2;
+	    double z = 3;
+	    double a = 4;
+	    double b = 5;
+	    double c = 6;
+	    
+	 //   System.out.println("(Using the static function) The scalar product of the vector (1,2,3) is: " +tv1.scalarProduct());
 	 
 	}
 
