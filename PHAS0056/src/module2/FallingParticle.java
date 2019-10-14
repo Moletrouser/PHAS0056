426ac55d805327c8bf9f8c0e1c5eca351bc8840e
Moletrouser <peter8rown@hotmail.co.uk>
2019-10-14 04:03:42 +0100

    added setH/getZ/setV/getV/getT

--- PHAS0056/src/module2/FallingParticle.java
+++ PHAS0056/src/module2/FallingParticle.java
@@ -1,7 +1,43 @@
 package module2;
 
 public class FallingParticle {
-
+	
+	double m = 1;
+	double d = 2;
+	double t;
+	double z;
+	double h;
+	double v;
+	public static final double g = -9.81;
+	
+	public FallingParticle(double m, double d) {
+		
+	}
+	
+	public double setH(double h) {
+		h = this.h;
+		return h;
+	}
+		
+	public double getZ() {
+		return z;
+	}
+	
+	public double setV(double v) {
+		v = this.v;
+		return v;
+	}
+	
+	public double getV() {
+		return v;
+	}
+	
+	public double getT() {
+		return t;
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
