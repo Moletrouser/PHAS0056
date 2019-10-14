20f478dfeeb448cecdef703a167bf3fbada67a80
Moletrouser <peter8rown@hotmail.co.uk>
2019-10-14 07:29:53 +0100

    added comments to TestComplex class

--- PHAS0056/src/module2/TestComplex.java
+++ PHAS0056/src/module2/TestComplex.java
@@ -14,6 +14,7 @@ public class TestComplex {
 		return imag;
 	}
 	
+	//gives modulus of the complex number passed to it
 	public double modulus() {
 		double modulusOutput = Math.sqrt(real*real + imag*imag);
 		return modulusOutput;
@@ -24,17 +25,19 @@ public class TestComplex {
 		double angle = Math.toDegrees(Math.acos(real/imag));
 		return angle;
 	}
-	
+	//gives complex conjugate of the complex number passed to it
 	public Complex conjugate() {
 		Complex conj = new Complex(real, -imag);
 		return conj;
 	}
 	
+	//normalises the complex number passed to it
 	public Complex normalised() {
 		Complex norm = new Complex(real/modulus(), imag/modulus());
 		return norm;
 	}
 	
+	// returns true or false depending on whether the two complex numbers passed to it match
 	public boolean equals(Complex c) {
 		if (c.real == this.real && c.imag == this.imag) {  
 		return true;
@@ -42,7 +45,7 @@ public class TestComplex {
 		return false;
     }
 	
-	// ang is given in degrees
+	// takes the modulus and argument and outputs the complex number they correspond to, ang is given in degrees
 	public static Complex setFromModulusAngle(double mag, double ang) {
 		double real = Math.cos(Math.toRadians(ang))*mag;
 		double imag = Math.sin(Math.toRadians(ang))*mag;
@@ -50,17 +53,19 @@ public class TestComplex {
 		return complex1;
 	}
 	
+	// adds the two complex numbers passed to it
 	public static Complex add(Complex c1, Complex c2) {
 		Complex newc = new Complex(c1.real + c2.real, c1.imag + c2.imag);
 		return newc;
 		
 	}
-	
+	// subtracts c2 from c1
 	public static Complex subtract(Complex c1, Complex c2) {
 		Complex newc = new Complex(c1.real - c2.real, c1.imag - c2.imag);
 		return newc;
 	}
 	
+	// multiplies the two complex numbers passed to it
 	public static Complex multiply(Complex c1, Complex c2) {
 		double newreal = (c1.real*c2.real) - (c1.imag*c2.imag);
 		double newimag = (c1.real*c2.imag) + (c2.real*c1.imag);
