01c13eb99f98e93e0cbc3313b96985dfc1f9bd46
Moletrouser <peter8rown@hotmail.co.uk>
2019-10-14 07:24:36 +0100

    added comments to Complex class

--- PHAS0056/src/module2/Complex.java
+++ PHAS0056/src/module2/Complex.java
@@ -5,22 +5,23 @@ public class Complex {
 	double real;
 	double imag;
 	
-	
-	
 	public Complex(double real, double imag) {
 		this.real = real;
 		this.imag = imag;
 		
 	}
 	
+	// defines how java should return the real part of a complex number
 	public double real() {
 		return real;
 	}
 	
+	// defines how java should return the imaginary part of a complex number
 	public double imag() {
 		return imag;
 	}
 	
+	// returns the modulus of the complex number passed to it
 	public double modulus() {
 		double modulusOutput = Math.sqrt(real*real + imag*imag);
 		return modulusOutput;
@@ -32,16 +33,19 @@ public class Complex {
 		return angle;
 	}
 	
+	// returns the complex conjugate of the complex number passed to it
 	public Complex conjugate() {
 		Complex conj = new Complex(real, -imag);
 		return conj;
 	}
 	
+	// normalises the complex number passed to it
 	public Complex normalised() {
 		Complex norm = new Complex(real/modulus(), imag/modulus());
 		return norm;
 	}
 	
+	// returns true or false depending on whether the two complex numbers passed to it match
 	public boolean equals(Complex c) {
 		if (c.real == this.real && c.imag == this.imag) {  
 		return true;
@@ -49,7 +53,8 @@ public class Complex {
 		return false;
     }
 	
-	// ang is given in degrees
+	
+	// takes the modulus and argument and outputs the complex number they corespond to, ang is given in degrees
 	public static Complex setFromModulusAngle(double mag, double ang) {
 		double real = Math.cos(Math.toRadians(ang))*mag;
 		double imag = Math.sin(Math.toRadians(ang))*mag;
@@ -57,17 +62,20 @@ public class Complex {
 		return complex1;
 	}
 	
+	// adds the two complex numbers passed to it
 	public static Complex add(Complex c1, Complex c2) {
 		Complex newc = new Complex(c1.real + c2.real, c1.imag + c2.imag);
 		return newc;
 		
 	}
 	
+	// subtracts the two complex numbers passed to it, subtracts c2 from c1
 	public static Complex subtract(Complex c1, Complex c2) {
 		Complex newc = new Complex(c1.real - c2.real, c1.imag - c2.imag);
 		return newc;
 	}
 	
+	// multiplies the two complex numbers passed to it
 	public static Complex multiply(Complex c1, Complex c2) {
 		double newreal = (c1.real*c2.real) - (c1.imag*c2.imag);
 		double newimag = (c1.real*c2.imag) + (c2.real*c1.imag);
@@ -75,7 +83,7 @@ public class Complex {
 		return newcomplex;
 	}
 	
-	// c1 divided by c2
+	// divides the complex number c1 by c2
 	public static Complex divide(Complex c1, Complex c2) {
 		double realnumerator = c1.real*c2.real + c1.imag*c2.imag;
 		double imagnumerator = c2.real*c1.imag - c1.real*c2.imag;
@@ -86,7 +94,7 @@ public class Complex {
 	
 	
 	
-	
+	// tells java how to display complex numbers as strings
 	public String toString() {
 		String realString = Double.toString(real);
 		String imagString = Double.toString(imag);
@@ -107,22 +115,6 @@ public class Complex {
 		Complex I = new Complex(0,1);
 		Complex c1 = new Complex(1,-2);
 		Complex c2 = new Complex(-2,1);
-		
-		
-		
-		/* 
-		 * The following commented out code is for my testing only
-		 * 
-		Complex i1 = new Complex(1,2);
-		Complex i2 = new Complex(3,4);
-		System.out.println(i1.argument());
-		System.out.println(i1.equals(i2));
-		System.out.println(setFromModulusAngle(3, 45));
-		System.out.println("1,2 plus 3,4 is: " +add(i1,i2));
-		System.out.println("1,2 minus 3,4 is: " +subtract(i1,i2));
-		System.out.println("1,2 multiplied by 3,4 is: " +multiply(i1,i2));
-		System.out.println("1,2 divided by 3,4 is: " +divide(i1,i2));
-		*/
 		
 		System.out.println("The product of c1 and c2 is: " +multiply(c1,c2));
 		System.out.println("The ratio of c1 to c2 is: " +divide(c1,c2));
