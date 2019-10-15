10efe3595f145dbae32105424b4b71d01c488fd4
Peter Brown <peter.brown.16@ucl.ac.uk>
2019-10-15 21:20:47 +0100

    Copied complex,fallingparticle and threevector over to module3

---
+++ PHAS0056/src/module3/Complex.java
@@ -0,0 +1,138 @@
+package module3;
+
+public class Complex {
+
+	double real;
+	double imag;
+	
+	public Complex(double real, double imag) {
+		this.real = real;
+		this.imag = imag;
+		
+	}
+	
+	// defines how java should return the real part of a complex number
+	public double real() {
+		return real;
+	}
+	
+	// defines how java should return the imaginary part of a complex number
+	public double imag() {
+		return imag;
+	}
+	
+	// returns the modulus of the complex number passed to it
+	public double modulus() {
+		double modulusOutput = Math.sqrt(real*real + imag*imag);
+		return modulusOutput;
+	}
+	
+	// gives the argument of a complex number in degrees
+	public double argument() {
+		double angle = Math.toDegrees(Math.acos(real/imag));
+		return angle;
+	}
+	
+	// returns the complex conjugate of the complex number passed to it
+	public Complex conjugate() {
+		Complex conj = new Complex(real, -imag);
+		return conj;
+	}
+	
+	// normalises the complex number passed to it
+	public Complex normalised() {
+		Complex norm = new Complex(real/modulus(), imag/modulus());
+		return norm;
+	}
+	
+	// returns true or false depending on whether the two complex numbers passed to it match
+	public boolean equals(Complex c) {
+		if (c.real == this.real && c.imag == this.imag) {  
+		return true;
+	}
+		return false;
+    }
+	
+	
+	// takes the modulus and argument and outputs the complex number they corespond to, ang is given in degrees
+	public static Complex setFromModulusAngle(double mag, double ang) {
+		double real = Math.cos(Math.toRadians(ang))*mag;
+		double imag = Math.sin(Math.toRadians(ang))*mag;
+		Complex complex1 = new Complex(real,imag);
+		return complex1;
+	}
+	
+	// adds the two complex numbers passed to it
+	public static Complex add(Complex c1, Complex c2) {
+		Complex newc = new Complex(c1.real + c2.real, c1.imag + c2.imag);
+		return newc;
+		
+	}
+	
+	// subtracts the two complex numbers passed to it, subtracts c2 from c1
+	public static Complex subtract(Complex c1, Complex c2) {
+		Complex newc = new Complex(c1.real - c2.real, c1.imag - c2.imag);
+		return newc;
+	}
+	
+	// multiplies the two complex numbers passed to it
+	public static Complex multiply(Complex c1, Complex c2) {
+		double newreal = (c1.real*c2.real) - (c1.imag*c2.imag);
+		double newimag = (c1.real*c2.imag) + (c2.real*c1.imag);
+		Complex newcomplex = new Complex(newreal, newimag);
+		return newcomplex;
+	}
+	
+	// divides the complex number c1 by c2
+	public static Complex divide(Complex c1, Complex c2) {
+		Complex ZERO = new Complex(0,0);
+		double realnumerator = c1.real*c2.real + c1.imag*c2.imag;
+		double imagnumerator = c2.real*c1.imag - c1.real*c2.imag;
+		double denominator = c2.real*c2.real + c2.imag*c2.imag;
+		Complex newcomplex = new Complex(realnumerator/denominator, imagnumerator/denominator);
+		if (newcomplex==ZERO) {
+		      throw new Exception("Trying to divide by zero");
+		    }
+		   
+		return newcomplex;
+		    }
+	
+	
+	
+	
+	// tells java how to display complex numbers as strings
+	public String toString() {
+		String realString = Double.toString(real);
+		String imagString = Double.toString(imag);
+		String stringComplex = "(" + realString + ", " + imagString + "i" +")";
+		return stringComplex;
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		Complex ONE = new Complex(1,0);
+		Complex ZERO = new Complex(0,0);
+		Complex I = new Complex(0,1);
+		Complex c1 = new Complex(1,-2);
+		Complex c2 = new Complex(-2,1);
+		
+		System.out.println("The product of c1 and c2 is: " +multiply(c1,c2));
+		System.out.println("The ratio of c1 to c2 is: " +divide(c1,c2));
+		System.out.println("The  product of c1 and I: " +multiply(c1,I));
+		System.out.println("The ratio of c1 to zero is: " +divide(c1,ZERO));
+		System.out.println("The product of c1 with its conjugate is: " +multiply(c1,c1.conjugate()));
+		System.out.println("The product of c2 with its conjugate is: " +multiply(c2,c2.conjugate()));
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ PHAS0056/src/module3/ThreeVector.java
@@ -0,0 +1,141 @@
+package module3;
+
+public class ThreeVector {
+	
+	double x;
+	double y;
+    double z;
+    double a;
+    double b;
+    double c;
+	
+	public ThreeVector(double x, double y, double z) {
+		this.x = x; 
+		this.y = y;
+		this.z = z;
+		
+		
+	}
+	// calculates the magnitude of the 3 vector passed to it
+	public double magnitude() {
+		double magnitudeOutput;
+		magnitudeOutput = Math.sqrt((x*x)+(y*y)+(z*z));
+		return magnitudeOutput;
+	
+	}
+	
+	// calculates the unit vector of the three vector passed to it
+	public ThreeVector unitVector() {
+		ThreeVector unit = new ThreeVector(x/magnitude(), y/magnitude(), z/magnitude());
+		return unit;
+	}
+	
+	// calculates the sum of two vectors (static version)
+	public  static ThreeVector addVector(ThreeVector vector1, ThreeVector vector2) {
+		ThreeVector newVector = new ThreeVector(vector1.x+vector2.x, vector1.y+vector2.y, vector1.z+vector2.z);
+		return newVector;
+	
+    }
+	
+	// calculates the angle between two three vectors (static version)
+	public static double angle(ThreeVector vector1, ThreeVector vector2) {
+		double angleOutput;
+		angleOutput = Math.toDegrees(Math.acos(scalarProduct(vector1, vector2)/(vector1.magnitude()*vector2.magnitude())));
+		return angleOutput;
+	
+	}
+	
+	// overwrites the java toString function to tell it how to print vectors legibly 
+	public String toString() {
+		String xString = Double.toString(x);
+		String yString = Double.toString(y);
+		String zString = Double.toString(z);
+		String stringVector = "(" + xString + ", " + yString + ", " + zString + ")";
+		return stringVector;
+	
+	}
+	
+	// calculates the scalar product of the two three vectors passed to it (static version)
+	public static double scalarProduct(ThreeVector vector1, ThreeVector vector2) {
+		double scalarOutput;
+		scalarOutput = vector1.x*vector2.x + vector1.y*vector2.y + vector1.z*vector2.z;
+		return scalarOutput;
+	
+	}
+	
+	// calculates the scalar product of the two three vectors passed to it (non-static version)
+	public double scalarProduct(ThreeVector v2) {
+		return scalarProduct(this,v2);
+	}
+	
+	// calculates the vector product of the two three vectors passed to it (static version)
+    public static ThreeVector vectorProduct(ThreeVector vector1, ThreeVector vector2) {
+    	double vectorOutputx = (vector1.y*vector2.z)-(vector1.z*vector2.y);
+    	double vectorOutputy = (vector1.z*vector2.x)-(vector1.x*vector2.z);
+    	double vectorOutputz = (vector1.x*vector2.y)-(vector1.y*vector2.x);
+    	ThreeVector vectorOutput = new ThreeVector(vectorOutputx, vectorOutputy, vectorOutputz);
+    	return vectorOutput;
+    	
+    }
+	
+    // calculates the vector product of the two three vectors passed to it (non-static version)
+    public ThreeVector vectorProduct(ThreeVector v2) {
+    	return vectorProduct(this, v2);
+    
+    }
+    // calculates the angle between the two three vectors passed to it (non-static version)
+    public double angle(ThreeVector v2) {
+    	return angle(this, v2);
+    }
+    
+    // calculates the sum of the two three vectors passed to it (non-static version)
+    public ThreeVector addVector(ThreeVector v2) {
+    	return addVector(this, v2);
+    }
+    	
+  
+    
+    
+    
+    
+	public static void main(String[] args) {
+		
+		
+		ThreeVector v1 = new ThreeVector(4,5,3);
+		ThreeVector v2 = new ThreeVector(1,4,2);
+		ThreeVector v3 = new ThreeVector(0,0,0);
+		
+		System.out.println("Vector v1 is: " +v1);
+    	System.out.println("Vector v2 is: " +v2);
+    	System.out.println("Vector v3 is: " +v3);
+    	System.out.println("\n");
+    	
+    	System.out.println("The unit vector of vector v1 is: " +v1.unitVector());
+    	System.out.println("The unit vector of vector v2 is: " +v2.unitVector());
+    	System.out.println("The unit vector of vector v3 is: " +v3.unitVector());
+    	System.out.println("\n");
+    	
+    	System.out.println("(STATIC) The scalar product of v1 and v2 is: " +scalarProduct(v1,v2));
+    	System.out.println("(STATIC) The scalar product of v1 and v3 is: " +scalarProduct(v1,v3));
+    	System.out.println("(STATIC) The vector product of v1 and v2 is: " +vectorProduct(v1,v2));
+    	System.out.println("(STATIC) The vector product of v1 and v3 is: " +vectorProduct(v1,v3));
+    	System.out.println("(STATIC) The angle between v1 and v2 is: " +angle(v1,v2));
+     	System.out.println("(STATIC) The angle between v1 and v3 is: " +angle(v1,v3));
+     	System.out.println("\n");
+    	
+    	System.out.println("(NON-STATIC) The scalar product of v1 and v2 is: " +v1.scalarProduct(v2));
+    	System.out.println("(NON-STATIC) The scalar product of v1 and v3 is: " +v1.scalarProduct(v3));
+    	System.out.println("(NON-STATIC) The vector product of v1 and v2 is: " +v1.vectorProduct(v2));
+    	System.out.println("(NON-STATIC) The vector product of v1 and v3 is: " +v1.vectorProduct(v3));
+    	System.out.println("(NON-STATIC) The angle between v1 and v2 is: " +v1.angle(v2));
+     	System.out.println("(NON-STATIC) The angle between v1 and v3 is: " +v1.angle(v3));
+    	System.out.println("\n");
+    	
+		System.out.println("If we comment out the toString method and then try and print vector v1 we get the output: ThreeVector@34c45dca");
+		System.out.println("This is because java already has a definition of the toString function built into it, and unless we specify a different definition it will default to it's built in definition.");
+		System.out.println("34c45dca is a hexidecimal number, probably refering to the memory address at which the v1 variable is stored in the stack.");
+		System.out.println("Since java does not now how to print the format of number we have given it, it seems to instead default to printing the memory address at which the variable is stored");
+	}
+	
+
+}
