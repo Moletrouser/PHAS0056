1c275a8d517ce9d0cb256426b5c5ac209e33ccb9
Moletrouser <peter8rown@hotmail.co.uk>
2019-10-21 06:07:00 +0100

    finished alphabet class and added comments

--- PHAS0056/src/module3/Alphabet.java
+++ PHAS0056/src/module3/Alphabet.java
@@ -2,103 +2,88 @@ package module3;
 
 	import java.util.Random;
 	
-	
-	
 public class Alphabet {
 	
 	int n;
 	String builtString = "";
-	
+
 	public String StringBuilder(String newchar) {
 		builtString = builtString + newchar;
 		return builtString;
 	}
 	
-	public static char randomCharacter() { // throws Exception{
+	// generates a random character by selecting a character with ASCII number between 0-128, warning may generate unprintable character
+	public static char randomCharacter() { 
 		Random random = new Random();
 		int n = random.nextInt(128);
-		//if (n<=31) {
-		//	throw new Exception("The ASCII symbol rolled was not a letter or a digit");
-		//}
 		char character = (char)n;
 	return character;
-		
 	}
+	
+	// converts both letters and numbers to integers, throws exception if non letter/non number detected as input
 	public static int parseInt(String s) throws Exception {
 		char charconvert = s.charAt(0);
-		//boolean digittruth = Character.isDigit(charconvert);
-		//boolean lettertruth = Character.isLetter(c);
-		//if (digittruth == false && lettertruth == false) {
-		//	throw new Exception("The character:" +charconvert +" is neither a letter or a digit");
-		//}
 		int parsedint = (int)charconvert;
-		if (parsedint <= 31 || parsedint == 127) {
-			throw new Exception("The character with ASCII number:" +parsedint +" is neither a letter nor a digit");
-		
+		if (Character.isDigit(charconvert) == false && Character.isLetter(charconvert)==false) {
+			throw new Exception("The character with ASCII number: " +parsedint +" is neither a letter nor a digit");
 		}
 		return parsedint;
 	}
 	
 	
-	
 	public static void main(String[] args) {
 		
-		/*
-		try {
-			System.out.println("The rolled random character was: " +randomCharacter());
-		}
-		catch(Exception e) {
-			System.out.println("Error: " +e.getMessage());
-		}
-		*/
 		
+		// initialises the loop counter, empty string, running total and number of exceptions caught as zero
 		int i = 0;
 		String builtstring = "";
 		int runningtotal = 0;
 		int exceptions = 0;
 		
-		while (i<=400) {
+		// will loop 400 times
+		while (i<400) {
 			
 			try {
+			
 			char c = randomCharacter();
 			boolean lettertruth = Character.isLetter(c);
 			boolean digittruth = Character.isDigit(c);
-			if (lettertruth == true || digittruth == true) {
+			
+			//checks if rolled character is a letter
+			if (lettertruth == true) {
 				String converttostring = Character.toString(c);
-				//int converttoint = (int) c;
 				int converttoint = parseInt(converttostring);
 				builtstring = builtstring + converttostring;
 				runningtotal = runningtotal + converttoint;
 			}
-			String converttostring = Character.toString(c);
-			int converttoint = parseInt(converttostring);
-			/*
-			if (lettertruth == true) {
-				//int charconvert = (int)c;
-				String stringconvert = Character.toString(c);
-				int stringtonumber = parseInt(stringconvert);
-				runningtotal = runningtotal + stringtonumber;S
-				builtstring = builtstring + stringconvert;
+			//checks if rolled character is a digit
+			if (digittruth == true) {
+				String converttostring = Character.toString(c);
+				int converttoint = parseInt(converttostring);
+				builtstring = builtstring + converttostring;
+				runningtotal = runningtotal + converttoint;
 			}
-			//if (digittruth == true) {
-			if (lettertruth = false) {
-				String stringconvert = Character.toString(c);
-				int intconvert = parseInt(stringconvert);
-				runningtotal = runningtotal + intconvert;
-				builtstring = builtstring + stringconvert;
+			else {
+				String converttostring = Character.toString(c);
+				int converttoint = parseInt(converttostring);
+				parseInt(converttostring);
+				//System.out.println(converttoint);
 			}
-			*/
-			
 			}
+			// prints error message if exception detected and adds 1 to exception counter
 			catch(Exception e) {
 				System.out.println("Error: " +e.getMessage());
-				exceptions = exceptions + 1;
-				
+				exceptions = exceptions + 1;	
 			}
-			
+			finally {
+			// advances the loop counter by 1
 			i = i + 1;
+			}
 		}
+		
+		int length = builtstring.length();
 		System.out.println(builtstring);	
+		System.out.println("The length of the built string is: " +length);	
 		System.out.println("The total of numbers added together is: " +runningtotal);
 		System.out.println("The total of numbers of exceptions was: " +exceptions);
 			
