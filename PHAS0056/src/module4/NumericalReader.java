a82b6216e5c0d9022aa367fa888f6033c834b946
Moletrouser <peter8rown@hotmail.co.uk>
2019-10-27 23:48:06 +0000

    added analsisStart/data/get string methods

--- PHAS0056/src/module4/NumericalReader.java
+++ PHAS0056/src/module4/NumericalReader.java
@@ -23,24 +23,53 @@ public class NumericalReader {
 	}
 	
 	void analysisStart(String dataFile) throws IOException {
-		double minValue;
-		double maxValue;
-		double nValues;
-		double sumOfValues;
+		double minValue = 0;
+		double maxValue = 10000;
+		double nValues  = 0;
+		double sumOfValues = 0;
 		FileWriter f = new FileWriter(dataFile);
 		f.close();
 	}
 	
-	void analyseData(String line) {
+	void analyseData(String line) throws IOExceptions {
+		double minValue = 0;
+		double maxValue = 100000;
+		double nValues  = 0;
+		double sumOfValues = 0;
 		Character firstchar =  line.charAt(0);
 		Scanner sc = new Scanner(line);
-		if (line != null && Character.isLetter(firstchar) == false) {
-			while(sc.hasNext()){
-		       // sc.next();
-		        System.out.println(sc.next());
-		sc.close();		
-		}
-		}
+		
+		
+		
+		while(sc.hasNext()){
+			if (line != null && Character.isLetter(firstchar) == false) {
+		    System.out.println("The next number on the line is: " +sc.nextInt());
+			double nextnumber = sc.nextInt();
+			
+		        if (nextnumber > minValue) {
+		        System.out.println("The next number is: " +nextnumber);
+		        minValue = nextnumber;
+		        FileWriter f = new FileWriter("fileName");
+		        BufferedWriter b = new BufferedWriter(f);
+		        PrintWriter pw = new PrintWriter(b);
+		        pw.println(nextnumber);
+		        pw.close();
+		        }
+		        
+		        if (nextnumber < maxValue) {
+		        System.out.println("The next number is: " +nextnumber);
+		        maxValue = nextnumber;
+		        FileWriter f = new FileWriter("fileName");
+		        BufferedWriter b = new BufferedWriter(f);
+		        PrintWriter pw = new PrintWriter(b);
+		        pw.println(nextnumber);
+		        pw.close();
+		        }
+		        
+	  nValues = nValues + 1;
+	  }
+	  sc.close();		
+	  }
 	
 	
 	}
@@ -52,15 +81,21 @@ public class NumericalReader {
 		
 		
 		// C:\Users\Peter Brown\git\PHAS0056\PHAS0056\src\module4
-		String directory = getStringFromKeyboard();
-		String saveFile = (directory + File.separator + dataFile);
+		//String directory = getStringFromKeyboard();
+		//String saveFile = (directory + File.separator + dataFile);
+		
+		
 		
 		NumericalReader nr = new NumericalReader();
 		
+		
+		//http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data1.txt
 		try { 
+		String directory = getStringFromKeyboard();
+		String saveFile = (directory + File.separator + dataFile);
 		String urlString = getStringFromKeyboard();
 		BufferedReader b = nr.brFromURL(urlString);
-		nr.analysisStart();
+		nr.analysisStart(saveFile);
 		
 		}
 		
