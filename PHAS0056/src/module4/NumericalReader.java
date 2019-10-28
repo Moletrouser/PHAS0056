49308118c9f594db7e66a6a37494e886eaf0ead1
Moletrouser <peter8rown@hotmail.co.uk>
2019-10-28 05:04:04 +0000

    added comments to wordCounter and NumericalReader ready for submission

--- PHAS0056/src/module4/NumericalReader.java
+++ PHAS0056/src/module4/NumericalReader.java
@@ -1,8 +1,8 @@
 package module4;
 
 import java.io.*;
-import java.net.*;	
-import java.util.Scanner; 
+import java.net.*;
+import java.util.Scanner;
 
 public class NumericalReader {
 	
@@ -12,6 +12,7 @@ public class NumericalReader {
 	private double sumOfValues;
 	private PrintWriter p;
 	
+	// takes user input from the keyboard and returns it as a string
 	public static String getStringFromKeyboard() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter a string:");
@@ -20,136 +21,136 @@ public class NumericalReader {
 		return inputstring;
 	}
 	
-	public BufferedReader brFromURL(String urlName) throws IOException {
-		URL newurl = new URL(urlName);
+	// writes the text from the URL page into a buffer in memory
+	// public BufferedReader brFromURL(URL url) {
+	public static BufferedReader brFromURL (URL url) {
 		InputStream is = null;
 		try {
-		is = newurl.openStream();
-		}
-		catch (IOException e){
-		System.out.println("Error with URL: Check that the entered URL is formatted correctly");
-		}
-		InputStreamReader r = new InputStreamReader(is);
-		BufferedReader b = new BufferedReader(r);
+			is = url.openStream();
+		} catch (IOException e) {
+			System.out.println("Error with URL, check URL is formatted correctly. " +e);
+			}
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader b = new BufferedReader(isr);
 		return b;
-	}
-	
-	void analysisStart(String dataFile) throws IOException {
-		FileWriter f = null;
-		File output = new File("C:\\mywork\\numbers.txt");
-		//FileWriter f = new FileWriter(dataFile);
-		try {
-			f = new FileWriter(output);
 		}
-		catch (IOException e) {
-			System.out.println("Cannot write to file, check target directory.");
+
+	
+	// initialises min/max values and numberof/sumof values to later be updated by analyseData
+	// void analysisStart(String dataFile) {
+	public void analysisStart(String saveFile) {
+		//File outputfile = new File("C:\\mywork\\numbers.txt");
+		File outputfile = new File(saveFile);
+		FileWriter f = null;
+		try {
+			f = new FileWriter(outputfile);
+		} catch (IOException e) {
+			System.out.println("Cannot write to the file, check directory is correct. " +e);
 		}
 		BufferedWriter b = new BufferedWriter(f);
 		p = new PrintWriter(b);
-		minValue = 0;
-		maxValue = 10000;
-		nValues = 0;
+		minValue = 1000; 
+		maxValue = -1000; 
+		nValues = 0; 
 		sumOfValues = 0;
 	}
 	
-	void analyseData(String line)  throws IOException {
-		
-		Character firstchar =  line.charAt(0);
-		String trimstring = line.trim(); //removes leading and trailing spaces from string
-		Scanner sc = new Scanner(trimstring);
-		while(sc.hasNextDouble()){
-			double nextnumber = sc.nextDouble();
-			String stringconvert = Double.toString(nextnumber);
-			//if (Character.isDigit(firstchar) == true) {
-		    System.out.println("The next number on the line is: " +sc.nextDouble());
-		    if (nextnumber > minValue) {
-		        System.out.println("The current minValue is: " +nextnumber);
-		       	minValue = nextnumber;		      
-		    }
-		        
-		    if (nextnumber < maxValue) {
-		        System.out.println("The current maxValue is: " +nextnumber);
-		        maxValue = nextnumber;
-		   }
-		        
-	   nValues = nValues + 1;
-	   sumOfValues = sumOfValues + nextnumber;
-	   System.out.println("The current nValue is: " +nValues);
-	   System.out.println("The current sumOfValues is: " +nValues);
-	   }		
-   }
+	// creating method which will analyse the data in the URL file ignoring blank line and 
+	// comment lines. It will adjust minVal, maxVal,no. of Values and sum of Values accordingly
+	// and print lines containing numbers to screen and to numbers.txt file
+	public void analyseData(String string) {
+		String trimmedstring = string.trim();
+		Scanner sc = new Scanner(trimmedstring);  //trims the string by removing any leading or trailing white space
+		while (sc.hasNextDouble()) {    //loops while the scanner still has a double on the line given as input
+			Double nextnumber = sc.nextDouble();
+		String stringconvert = Double.toString(nextnumber);
+		if (minValue > nextnumber) { //tests if the current value is a new minimum value
+			minValue = nextnumber;
+		}
+		if (maxValue < nextnumber) { //tests if the current value is a new maximum value
+			maxValue = nextnumber;
+		}
+		nValues = nValues + 1; 
+		sumOfValues = sumOfValues + nextnumber;
+		System.out.println(stringconvert);
+		p.println(stringconvert);
+		//sc.close();
+		}
+	}
 	
-	
-	
-	
+	// creating method which will finish analysis by printing to screen the final minVal,
+	// maxVal, no. of values and average value. It will also finish writing to numbers.txt file.
 	public void analysisEnd () {
 		double average = sumOfValues/nValues;
-		System.out.println("The minimum value is: " +minValue +"\n The maximum value is:" +maxValue+ "\n The average value is " + average +"\n The total number of values is: " +nValues +"\n");
+		System.out.println("\n");
+		System.out.println("The minimum value is: " +minValue);
+		System.out.println("\n");
+		System.out.println("The maximum value is: " +maxValue);	
+		System.out.println("\n");
+		System.out.println("The average value is: " +average);	
+		System.out.println("\n");
+		System.out.println("The total number of values is: " +nValues);	
+		System.out.println("\n");
 		p.close();
 	}
 	
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the file path of the directory you would like to save the numbers text files in: ");
+		String saveDir = NumericalReader.getStringFromKeyboard();
+		if (saveDir.length() == 0) {
+			saveDir = ("C:" +File.separator +"users" +File.separator +"Peter Brown"); // this is the home directory on my personal computer, i can change this to my UCL home 
+		}																			  // directory for demonstration purposes but it would not work for testing here
 
-	public static void main(String[] args) throws IOException {
+		String dataFile1 = "numbers1.txt";
+		String dataFile2 = "numbers2.txt";
+		String saveFile1 = (saveDir + File.separator + dataFile1);
+		String saveFile2 = (saveDir + File.separator + dataFile2);
 		
-		
-		
-		//String directory = getStringFromKeyboard();
-		//String saveFile = (directory + File.separator + dataFile);
-		
-		
-		
-		NumericalReader nr = new NumericalReader();
-		// C:\mywork
-		// C:\Users\Peter Brown\git\PHAS0056\PHAS0056\src\module4
-		// http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data1.txt
-		//String dataFile = ("C:" + File.separator + "mywork" + File.separator + "numbers.txt");
-		
-		String dataFile = "numbers.txt";
-		File outputfile = new File(dataFile);
-		FileWriter fw = new FileWriter(outputfile);
-		//System.out.println("Enter the directory you would like to save your file in:");
-		//String directory = getStringFromKeyboard();
-		//String saveFile = (directory + File.separator + dataFile);
-		//String saveFile = "C:\mywork\numbers.txt";
-		String saveFile = ("C:" +File.separator + "mywork" + File.separator + "numbers.txt");
-		System.out.println("Enter a URL");
-		String urlString = getStringFromKeyboard();
-		BufferedReader b = nr.brFromURL(urlString);
-		nr.analysisStart(saveFile);
-		String line = b.readLine();
-		System.out.println("The current line is: " +line);
-		
-		while (line != null) {
-			try {
-			nr.analyseData(line); // analyze lines, check for comments etc.
-			}
-			catch (IOException e) {
-				 System.out.println("Problem: "+e.getMessage());
-			}
+		NumericalReader nr1 = new NumericalReader ();
+		URL url1 = null; // initialises an empty URL
+		try {
+			url1 = new URL ("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data1.txt");  // this is the URL which will be read into the buffer
+			System.out.println("\n");
+			System.out.println("URL = "+url1); //prints the URL that has been used
+		} catch (MalformedURLException e) {
+			System.out.println("Please check that a valid URL address has been entered"+e);
 		}
-		fw.close();
+		BufferedReader b1 = brFromURL(url1);
+		String line1 = "";
 		
-		
-		
-		
-		
-		
-		
-		
-		/*
-		BufferedReader reader = nr.brFromURL("http://foo/bar/");
-		String line = "";
-
-		nr.analysisStart(saveFile); // initialize minValue etc.
-		while ((line = br.readLine()) != null) {
-			nr.analyseData(line); // analyze lines, check for comments etc.
+		nr1.analysisStart(saveFile1); 
+		try {
+			// read each line each line in succesion until last line is reached
+			while ((line1 = b1.readLine()) != null) { 
+				nr1.analyseData(line1); 
 			}
-		nr.analysisEnd(); // print min, max, etc.
-		*/
+		} catch (IOException e) {
+			System.out.println("Cannot read from file. " +e);
+		}
+		// prints maxValue, minValue, average and sumOfValues
+		nr1.analysisEnd();
+	
 		
-		
-		
-		
+		NumericalReader nr2 = new NumericalReader ();
+		URL url2 = null; // initialises an empty URL
+		try {
+			url2 = new URL ("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data2.txt"); // this is the URL which will be read into the buffer
+			System.out.println("\n");
+			System.out.println("URL = "+url2); //prints the URL that has been used
+		} catch (IOException e) {
+			System.out.println("Please check that a valid URL address has been entered"+e);
+		}
+		BufferedReader b2 = brFromURL(url2);
+		String line2 = "";
+		nr2.analysisStart(saveFile2); 
+		try {
+			while ((line2 = b2.readLine()) != null) { 
+				nr2.analyseData(line2); 
+			}
+		} catch (IOException e) {
+			System.out.println("Cannot read from file." +e);
+		}
+		nr2.analysisEnd(); // prints maxValue, minValue, average and sumOfValues
 	}
-
 }
--- PHAS0056/src/module4/WordCounter.java
+++ PHAS0056/src/module4/WordCounter.java
@@ -10,39 +10,27 @@ import java.util.Scanner;
 public class WordCounter {
 	
 	
-	
+	// reads the URL given as an argument into the buffer
 	public static BufferedReader brFromURL(String urlName) throws IOException {
 		URL newurl = new URL(urlName);
 		InputStream is = newurl.openStream();
 		InputStreamReader r = new InputStreamReader(is);
 		BufferedReader b = new BufferedReader(r);
-		//System.out.println("Please type something!");
-		//try {
-		//	String s = b.readLine();
-		//	System.out.println("You typed: "+s);
-		//	String line;
-		//	 while ((line=b.readLine()) != null) {
-		//	 System.out.println(line);
-		//	 }
-		//}
-		//catch (IOException e) {
-		//	System.out.println("Problem: "+e.getMessage());
-		//}	
 		return b;	
 	}
-		
+	// reads the file given as an argument into the buffer
 	public static BufferedReader brFromFile(String fileName) throws IOException {
 		FileReader fr = new FileReader(fileName);
 		BufferedReader br = new BufferedReader(fr);
 		return br;
 	}	
-	
+	// counts the number of words in the BufferedReader given as input
 	public static int countWordsInResource(BufferedReader dataAsBR) throws IOException {
 		try(Scanner sc = new Scanner(dataAsBR)){
-		    int count = 0;
-		    while(sc.hasNext()){
+		    int count = 0; //initialises the word count as zero
+		    while(sc.hasNext()){ // checks if the scanner has another word on that line
 		        sc.next();
-		        count = count + 1;
+		        count = count + 1; // adds one to the counter
 		    }
 		
 		return count;	    
@@ -55,9 +43,9 @@ public class WordCounter {
 	public static void main(String[] args) {
 		
 		try {
-		BufferedReader b = brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt");
-		int count = countWordsInResource(b);
-		System.out.println("The number of words on this webpage is: " +count);
+		BufferedReader b = brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt"); // this is the URL of the page that will be read into the buffer
+		int count = countWordsInResource(b);  // counts the words on the webpage
+		System.out.println("The number of words on this webpage is: " +count); // prints the word count
 		
 		}
 		catch(IOException e) {
