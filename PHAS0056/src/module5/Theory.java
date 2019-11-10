35d463848a9e8a6fcd4917730d20779215679893
Moletrouser <peter8rown@hotmail.co.uk>
2019-11-10 21:06:02 +0000

    added comments to the Theory class

--- PHAS0056/src/module5/Theory.java
+++ PHAS0056/src/module5/Theory.java
@@ -4,23 +4,14 @@ public class Theory {
 
 	public double n;
 	
-	public Theory(double nn) {
-		nn = n;
+	public Theory(double exponent) {
+		exponent = n;
 	}
 	
+	// defines the function x^n
 	public double y(double x) {
 		double y = Math.pow(x,n);
 		return y;
 	}
 	
-
-
-   public static void main(String[] args) {
-	
-	Theory t1 = new Theory(5);
-	
-	double value = t1.y(3);
-	System.out.println(value);
-	   
-   }
 }  
