2a232e526360895e00670180874284d1cca92e3a
Peter Brown <peter.brown.16@ucl.ac.uk>
2019-12-11 02:30:12 +0000

    finished commenting PrimeNumberTask

--- PHAS0056/src/module8/PrimeNumberTask.java
+++ PHAS0056/src/module8/PrimeNumberTask.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 
 public class PrimeNumberTask implements Runnable {
 	
-	ArrayList<Integer> primes = new ArrayList<Integer>();
-	
-	
+	ArrayList<Integer> primes = new ArrayList<Integer>(); // initialises the primes array list
+		
 	public boolean isPrime(int n) {
 		//check if n is a multiple of 2
 	    if (n%2==0) return false;
 	    //if not, then check the odds
-	    for(int i=3;i*i<=n;i+=2) {
+	    // only checks for factors of n up to root n 
+	    for(int i=3;i*i<=n;i+=2) { //since there can be no factors of size greater than root n
 	        if(n%i==0)
 	            return false;
 	    }
@@ -24,24 +24,15 @@ public class PrimeNumberTask implements Runnable {
 	public void run() {
         	for(int i = 0; i <= Double.POSITIVE_INFINITY; ++i) {
         		if (Thread.currentThread().isInterrupted()) return;
-        		if (isPrime(i)) {
-        			primes.add(i);
+        		if (isPrime(i)) { // checks if a number is prime
+        			primes.add(i); // if it is prime, it is added to an array list 
         		}
         	}         	
 	}
 	
 	
-	public long getPrimes() {
-		//System.out.println("The size of the primes array is: " +primes.size());
-		return primes.size();
+	public long getPrimes() { // a simple method to return the size of the primes array to check its 
+		return primes.size(); // working correctly and the array list is being populated
 	}
 	
-	
-	
-	
-	public static void main(String[] args) {
-		
-
-	}
-
 }
