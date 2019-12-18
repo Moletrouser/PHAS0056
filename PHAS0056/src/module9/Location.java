d65f4045fb7445a2f9335728cc7ed7955eb527a1
Peter Brown <peter.brown.16@ucl.ac.uk>
2019-12-18 20:27:53 +0000

    created Location class for getting and setting planet co-ords

---
+++ PHAS0056/src/module9/Location.java
@@ -0,0 +1,34 @@
+package module9;
+
+public class Location {
+
+	public int x;
+	public int y;
+	
+	Location(int x, int y){
+		
+		this.x = x;
+		this.y = y;
+		
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+		return;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+		return;
+	}
+	
+	
+}
