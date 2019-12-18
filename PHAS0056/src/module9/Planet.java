5f0afb9916051425b244a3544b1c8301a19e266c
Peter Brown <peter.brown.16@ucl.ac.uk>
2019-12-18 20:21:19 +0000

    created Planet class

---
+++ PHAS0056/src/module9/Planet.java
@@ -0,0 +1,25 @@
+package module9;
+
+import java.awt.Color;
+
+public class Planet {
+
+	String name;
+	double period;
+	Color colour;
+	int diameter;
+	int orbitRad;
+	double angularV;
+	double startAngle = 0;
+	
+	
+	public Planet(String name, int orbitRad, double period, Color colour, int diameter) {
+		
+		this.orbitRad = orbitRad;
+		this.name = name;
+		this.period = period;
+		this.angularV = 2*(Math.PI/this.period);
+		this.colour = colour;
+		
+	}
+}
