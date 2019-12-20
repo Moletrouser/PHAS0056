0a2188e7dce37bf09efaf0090430b2c8ef7aedd1
User <User@LAPTOP-J9GN3Q0C>
2019-12-20 06:09:34 +0000

    finished commenting location class

--- PHAS0056/src/module9/Location.java
+++ PHAS0056/src/module9/Location.java
@@ -1,10 +1,12 @@
 package module9;
 
 public class Location {
-
+	
+	// initialises the x and y coordinate variables
 	public int x;
 	public int y;
 	
+	// defines the constructor that takes x and y arguments
 	Location(int x, int y){
 		
 		this.x = x;
@@ -12,19 +14,21 @@ public class Location {
 		
 	}
 	
+	// getter method for x coordinate
 	public int getX() {
 		return this.x;
 	}
 	
+	 // getter method for y coordinate
 	public int getY() {
 		return this.y;
 	}
-	
+	// setter method for x coordinate
 	public void setX(int x) {
 		this.x = x;
 		return;
 	}
-	
+	// setter method for y coordinate
 	public void setY(int y) {
 		this.y = y;
 		return;
