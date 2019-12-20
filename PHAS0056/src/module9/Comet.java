ebc11f55420c119d02bdb16d9ad709c795fa94ab
User <User@LAPTOP-J9GN3Q0C>
2019-12-20 04:24:33 +0000

    added trueAnomaly and eccentricAnonaly calculations

--- PHAS0056/src/module9/Comet.java
+++ PHAS0056/src/module9/Comet.java
@@ -7,15 +7,16 @@ public class Comet {
 	public double eccentricity;
 	public String name;
 	public int majorAx;
+	public int minorAx;
 	public Color colour;
-	public int radius;
 	public double period;
 	public Location centreOfOrbit;
 	public double t;
 	double pericenter;
 	double avgRateOfSweep;
 	double timeAtPericenter;
-	double meanAnomoly;
+	double meanAnomaly;
+	int maxIter;
 	
 			
 	public Comet (String name, double eccentricity, int majorAx, Color colour, int radius, Location centreOfOrbit, double period, double avgRateOfSweep, double t, double timeAtPericenter) {
@@ -25,14 +26,31 @@ public class Comet {
 		this.centreOfOrbit = centreOfOrbit;
 		this.eccentricity = eccentricity;
 		this.majorAx = majorAx;
-		this.meanAnomoly = this.avgRateOfSweep*(this.t - this.timeAtPericenter);
+		this.meanAnomaly = this.avgRateOfSweep*(this.t - this.timeAtPericenter);
 		
 	}
 
-	public double getMeanAnomoly() {
-		return this.meanAnomoly;
+	
+
+	public double getMeanAnomaly() {
+		return this.meanAnomaly;
 	}
 	
+	
+	public double getEccentricAnomaly(double meanAnomaly, double e, int maxIter) {
+		int i = 0;
+		double x = meanAnomaly;
+		while(i<maxIter) {
+			x = meanAnomaly + (Math.sin(x)*e)-(e*x*Math.acos(x))/(1-e*Math.acos(x));
+		}
+		return x;
+	}
+	
+	double eccentricAnomaly = getEccentricAnomaly(meanAnomaly, eccentricity, 10);
+	
+	double trueAnomaly = Math.atan((Math.sqrt((1 + eccentricity)/(1 - eccentricity)))*Math.tan(eccentricAnomaly/2))*2;
+	
+	double radius = (majorAx * (1-Math.pow(eccentricity, 2)))/(1 + eccentricity*Math.acos(trueAnomaly));
 	
 	public String getName() {
 		return this.name;
@@ -40,10 +58,6 @@ public class Comet {
 	
 	public Color getColour() {
 		return this.colour;
-	}
-	
-	public int getRad() {
-		return this.radius;
 	}
 	
 	public double getE() {
